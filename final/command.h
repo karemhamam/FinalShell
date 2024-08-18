a0b9a6dfd7e70b5db06f93fd384818fe2e736219
karemhamam <56442491+karemhamam@users.noreply.github.com>
2024-08-18 11:00:49 +0300

    command.h

--- final/command.h
+++ final/command.h
@@ -1,4 +1,3 @@
-
 #ifndef COMMAND_H
 #define COMMAND_H
 
@@ -7,27 +6,22 @@
 #define MAX_HISTORY    (10)
 
 void prompt();
-void Data_IN(char *input , char **args);
+void Data_IN(char *input, char **args);
 void pwd(char **args);
-void echo (char **args);
-void cp (char **args);
-void mv (char **args);
-void help ( char **args);
-
+void echo(char **args);
+void cp(char **args);
+void mv(char **args);
+void help(char **args);
 void handle_piping(char **args);
 void handle_redirection(char **args);
 void free_command();
 void uptime_command();
 
-int myexit (char **args);
-int cd ( char **args);
-int type (char **args);
-int phist (char **args);
-int envir (char **args);
+int myexit(char **args);
+int cd(char **args);
+int type(char **args);
+int phist(char **args);
+int envir(char **args);
 int is_external(const char *command);
 
-
-
-
 #endif
-
